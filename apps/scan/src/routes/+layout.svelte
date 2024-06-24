e596da19ed20d06e9bde5b33bed0644ef7e540d7
MorelCorpJeff <jfmorel@gmail.com>
2024-06-23 21:03:14 -0400

    scan app shell starting to work

---
+++ apps/scan/src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let form;
+    import '../app.css';
+    import { onMount } from 'svelte';
+    // import { user } from '$lib/stores';
+    
+    // import { auth, provider, signInWithPopup } from '$lib/firebase';
+  
+    // onMount(() => {
+    //   auth.onAuthStateChanged((u) => {
+    //     user.set(u ? { uid: u.uid, displayName: u.displayName, email: u.email } : null);
+    //   });
+    // });
+  
+    // const login = async () => {
+    //   try {
+    //     await signInWithPopup(auth, provider);
+    //   } catch (error) {
+    //     console.error('Login failed:', error);
+    //   }
+    // };
+  
+    // const logout = async () => {
+    //   try {
+    //     await auth.signOut();
+    //   } catch (error) {
+    //     console.error('Logout failed:', error);
+    //   }
+    // };
+  </script>
+  
+  <slot {form} />
+  
+  <!-- {#if $user}
+    <button on:click={logout}>Logout</button>
+  {:else}
+    <button on:click={login}>Login with Google</button>
+  {/if}
+   -->
---
+++ apps/scan/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+  
+	const navigateToRegister = () => goto('/register');
+	const navigateToSale = () => goto('/sale');
+	const navigateToCheckout = () => goto('/checkout');
+  </script>
+  
+  <style>
+	main {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  height: 100vh;
+	  text-align: center;
+	}
+	.btn {
+	  margin: 1rem;
+	  padding: 1rem 2rem;
+	  font-size: 1.5rem;
+	  cursor: pointer;
+	}
+	.bazaar-btn {
+	  position: absolute;
+	  top: 1rem;
+	  right: 1rem;
+	}
+  </style>
+  
+  <main>
+	<button class="bazaar-btn" on:click={() => console.log('Open bazaar selection')}>Bazaar</button>
+	<h1>Welcome to Bazaar Scanner App</h1>
+	<button class="btn" on:click={navigateToRegister}>Register</button>
+	<button class="btn" on:click={navigateToSale}>Sale</button>
+	<button class="btn" on:click={navigateToCheckout}>Checkout</button>
+  </main>
+  
